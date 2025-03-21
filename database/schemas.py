175706c72d92c8fa8abe9b8d8188439fea4328e1
werte <wertewerte123@mail.ru>
2025-03-21 19:14:47 +0300

    -

--- database/schemas.py
+++ database/schemas.py
@@ -5,9 +5,9 @@ class AddUserSchema(BaseModel):
     tg_id: int
     username: str = Field(max_length=50)
     balance: int = Field(default=100)
-    is_superuser: bool = Field(default=False)
-    is_active: bool = Field(default=True)
-    jwt_refresh_token: str | None = Field(default=None)
+    is_superuser: int = Field(default=0, ge=0, le=1)
+    is_active: int = Field(default=1, ge=0, le=1)
+    jwt_refresh_token: str = Field(default="")
 
 
 class GetUserSchema(AddUserSchema):
